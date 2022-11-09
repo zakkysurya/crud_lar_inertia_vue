4a50dd44602535be4261a7a35b6acf4b9285a006
zakky surya <zakkysurya@gmail.com>
2022-11-10 06:14:49 +0700

    DatabaseSeeder - added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            ProfileSeeder::class,
+            SkillSeeder::class,
+            ExperienceSeeder::class,
+            EducationalSeeder::class,
+            ServiceSeeder::class,
+            PortfolioSeeder::class,
+            TestimonialSeeder::class,
+            MessageSeeder::class,
+        ]);
     }
 }
